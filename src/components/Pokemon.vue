e822db8b2cd772625318c0c276ce1c6b962451d8
Kelly Costa <kellydosocorro@gmail.com>
2020-04-16 03:22:33 -0300

    Adiciona paginação e componente Pokemon

---
+++ src/components/Pokemon.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="card">
+    <div class="card-header"><h4 class="my-0 font-weight-normal">{{ pokemon.name }}</h4></div>
+    <div class="card-body">
+      <div class="d-flex justify-content-between align-items-center">
+        <router-link v-if="showOpenButton" :to="`/pokemons/${pokemon.id}`">
+          <button type="button" class="btn btn-secondary">View</button>
+        </router-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "Pokemon",
+
+  props: {
+    pokemon: Object,
+    /* data() {
+      return {
+        name: '',
+        id: null,
+        images: [],
+        abilities: [],
+        moves: []
+      }
+    }, */
+    showOpenButton: {
+      type: Boolean,
+      default: false
+    },
+
+  }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,70 +1,70 @@
 <template>
-  <div class="home" style="margin-top:40px;">
+  <div class="home" style="margin-top:40px;" id="nav">
     <div class="container">
     <div class="row row-cols-1 row-cols-md-4">
       <div v-for="(pokemon, i) in pokemons" :key="i" class="col mb-4">
-        <div class="card">
-          <img src="" class="card-img-top" alt="">
-          <div class="card-header">
-            <h4 class="my-0 font-weight-normal">{{ pokemon.name }}</h4>
-          </div>
-          <div class="card-body">
-            <div class="d-flex justify-content-between align-items-center">
-                <div class="btn-group">
-                  <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                  <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                </div>
-            </div>
-          </div>
-        </div>
+        <Pokemon :pokemon="pokemon" :show-open-button="true"/>
       </div>
     </div>
 
     <nav aria-label="Page navigation example">
       <ul class="pagination justify-content-center">
         <li v-if="prev" class="page-item">
-          <a class="page-link" href="#" tabindex="-1">Previous</a>
+          <button v-on:click="prevPage()" class="page-link" href="#" tabindex="-1">Previous</button>
         </li>
         <li class="page-item">
-          <a class="page-link" href="#">Next</a>
+          <button v-on:click="nextPage()" class="page-link">Next</button>
         </li>
       </ul>
-      <p class="text-center">Mostrando {{this.next.substr(-2)}} de {{this.total}}</p>
     </nav>
-
-
-    <!--<ul>
-      <li v-for="(pokemon, i) in pokemons" :key="i">
-        {{ pokemon.name }}
-      </li>
-    </ul>-->
   </div></div>
 </template>
 
 <script>
 import axios from "axios";
+import Pokemon from "@/components/Pokemon";
 
 export default {
   name: "Home",
+
+  components: { Pokemon },
 
   data() {
     return {
       total: null,
       pokemons: [],
       next: '',
-      prev: Boolean
+      prev: Boolean,
+      current: 'https://pokeapi.co/api/v2/pokemon/?offset=0&limit=20'
     };
   },
 
-  async created() {
-    const res = await axios.get("https://pokeapi.co/api/v2/pokemon/");
-    this.pokemons = res.data.results;
-    this.next = res.data.next;
-    this.prev = res.data.previous;
-    this.total = res.data.count;
-    //console.log(this.pokemons)
+  methods: {
+    async getPokemons () {
+      await axios.get(this.current)
+        .then(res => {
+          this.pokemons = res.data.results;
+          this.next = res.data.next;
+          this.prev = res.data.previous;
+          this.total = res.data.count;
+        })
+        .catch(res => {
+          console.log(res);
+        });
+    },
+    nextPage () {
+      this.current = this.next;
+      this.getPokemons();
+    },
+    prevPage () {
+      this.current = this.prev;
+      this.getPokemons();
+    }
+  },
+  created () {
+    this.getPokemons();
   }
-};
+}
 </script>
 
 <style>
